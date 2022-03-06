0c59a6a63b710437d44d96f689ba60768fa6a62d
chriscardillo <cfcardillo23@gmail.com>
2022-03-06 17:25:19 -0500

    linting tests

--- tests/test_render_text.py
+++ tests/test_render_text.py
@@ -1,17 +1,14 @@
-import pytest
 from absql import render_text
 
 
 def test_render_text():
     template = "SELECT * FROM {{my_table_placeholder}}"
-    table_name = "my_table"
     result = render_text(template, my_table_placeholder="my_table")
     assert result == "SELECT * FROM my_table"
 
 
 def test_leave_unknown_vars_alone():
     template = "SELECT * FROM {{my_table_placeholder}} WHERE date = '{{ds}}'"
-    table_name = "my_table"
     result = render_text(template, my_table_placeholder="my_table")
     assert result == "SELECT * FROM my_table WHERE date = '{{ ds }}'"
 
