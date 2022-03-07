9e636fc251ac1360892ed430e4170a5a76ad8621
chriscardillo <cfcardillo23@gmail.com>
2022-03-06 21:46:18 -0500

    update tests

--- tests/test_render_text.py
+++ tests/test_render_text.py
@@ -8,15 +8,20 @@ def test_render_text():
 
 
 def test_leave_unknown_vars_alone():
-    template = "SELECT * FROM {{my_table_placeholder}} WHERE date = '{{ds}}'"
+    template = "SELECT * FROM {{my_table_placeholder}} WHERE date = '{{my_date}}'"
     result = r.render_text(template, my_table_placeholder="my_table")
-    assert result == "SELECT * FROM my_table WHERE date = '{{ ds }}'"
+    assert result == "SELECT * FROM my_table WHERE date = '{{ my_date }}'"
 
 
 # https://stackoverflow.com/questions/71374498/ignore-unknown-functions-in-jinja2
-
-# def test_leave_unknown_macros_alone():
-#     template = "SELECT * FROM {{my_table_placeholder}} WHERE date = '{{get_ds()}}'"
-#     table_name = "my_table"
-#     result = r.render_text(template, my_table_placeholder="my_table")
-#     assert result == "SELECT * FROM my_table WHERE date = '{{ get_ds() }}'"
+# Below is a workaround for a function that you don't want to run immediately
+def test_leave_unknown_functions_alone():
+    template = (
+        "SELECT * FROM {{my_table_placeholder}} WHERE date = '{{'{{ get_date() }}'}}'"
+    )
+    # The true test
+    # template = (
+    #     "SELECT * FROM {{my_table_placeholder}} WHERE date = '{{ get_ds() }}'"
+    # )
+    result = r.render_text(template, my_table_placeholder="my_table")
+    assert result == "SELECT * FROM my_table WHERE date = '{{ get_date() }}'"
