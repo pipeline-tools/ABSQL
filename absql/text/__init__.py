87d43f2d2b14a005fcaa49bf4594bec4ea08a747
chriscardillo <cfcardillo23@gmail.com>
2022-03-23 11:28:34 -0400

    add create_replacements function

--- absql/text/__init__.py
+++ absql/text/__init__.py
@@ -5,3 +5,11 @@ def clean_spacing(text):
     text = re.sub("\\{\\{\\s+", "{{ ", text)
     text = re.sub("\\s+\\}\\}", " }}", text)
     return text
+
+
+def create_replacements(**kwargs):
+    replacements = {}
+    for k, v in kwargs.items():
+        replacement = {"{{{{{k}}}}}".format(k=k): v, "{{{{ {k} }}}}".format(k=k): v}
+        replacements.update(replacement)
+    return replacements
--- tests/test_clean_text.py
+++ tests/test_clean_text.py
@@ -1,4 +1,4 @@
-from absql.text import clean_spacing
+from absql.text import clean_spacing, create_replacements
 
 
 def test_clean_spaces():
@@ -12,4 +12,10 @@ def test_clean_tabs():
     text = "{{  hello   }}   world"
     got = clean_spacing(text)
     want = "{{ hello }}   world"
+    assert got == want
+
+
+def test_replacements():
+    got = create_replacements(foo="bar")
+    want = {"{{foo}}": "bar", "{{ foo }}": "bar"}
     assert got == want
