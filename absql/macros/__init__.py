975ccf18764bc1aaa2a3b9bc6b28ee38ddca0d32
chriscardillo <cfcardillo23@gmail.com>
2022-03-25 19:06:53 -0400

    alphabetize default macros

--- absql/macros/__init__.py
+++ absql/macros/__init__.py
@@ -3,13 +3,10 @@ from .db import table_exists
 from datetime import datetime, timedelta
 
 
-# add query macro (accept string or sqlalchemy engine)
-# add env_switch macro
 default_macros = {
-    "env_var": env_var,
     "datetime": datetime,
-    "timedelta": timedelta,
-    "engine": None,
+    "env_var": env_var,
     "table_exists": table_exists,
+    "timedelta": timedelta,
 }
 default_constructors = {"!" + k: v for k, v in default_macros.items()}
