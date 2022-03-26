43a22288a9ea83afc6810c917b07095d87d4ba91
chriscardillo <cfcardillo23@gmail.com>
2022-03-26 09:59:08 -0400

    register query_db in defaults

--- absql/macros/__init__.py
+++ absql/macros/__init__.py
@@ -1,5 +1,5 @@
 from .env import env_var
-from .db import table_exists
+from .db import table_exists, query_db
 from datetime import datetime, timedelta
 
 
@@ -8,5 +8,6 @@ default_macros = {
     "env_var": env_var,
     "table_exists": table_exists,
     "timedelta": timedelta,
+    "query_db": query_db
 }
 default_constructors = {"!" + k: v for k, v in default_macros.items()}
