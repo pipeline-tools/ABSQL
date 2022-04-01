03d4435215caf83086fc0db362297a75f0ef188f
chriscardillo <cfcardillo23@gmail.com>
2022-04-01 18:00:49 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ABSQL",
-    version="0.1.2",
+    version="0.1.3",
     author="Chris Cardillo",
     author_email="cfcardillo23@gmail.com",
     description="A rendering engine for templated SQL",
