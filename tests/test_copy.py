e9b1443e980724b9d349cb2943fc00f5f24f8df8
chriscardillo <cfcardillo23@gmail.com>
2023-03-09 15:25:35 -0500

    fmt

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -3,10 +3,12 @@ import datetime
 from absql import Runner
 from copy import copy
 
+
 @pytest.fixture(scope="function")
 def r_original():
     # dt because I am scared to copy modules
     return Runner(f="foo", dt=datetime)
+
 
 @pytest.fixture(scope="function")
 def r_copy(r_original):
@@ -20,10 +22,10 @@ def test_copy_set_context(r_original, r_copy):
 
 
 def test_copy_set_attr(r_original, r_copy):
-    r_original.partial_kwargs = ['a']
-    r_copy.partial_kwargs = ['b']
-    assert r_original.partial_kwargs == ['a']
-    assert r_copy.partial_kwargs == ['b']
+    r_original.partial_kwargs = ["a"]
+    r_copy.partial_kwargs = ["b"]
+    assert r_original.partial_kwargs == ["a"]
+    assert r_copy.partial_kwargs == ["b"]
 
     r_copy.extra_context = {"f": "buzz"}
     assert r_original.render("{{f}}") == "foo"
