5ceea2df7919b177a87709bf88a39186bc4d82b9
Chris Cardillo <cfcardillo23@gmail.com>
2026-06-19 11:23:48 -0400

    try concat env var

--- tests/files/includes.sql
+++ tests/files/includes.sql
@@ -1,5 +1,5 @@
 {% if env_var("GITHUB_WORKSPACE") %}
-    {%- include '/home/runner/work/ABSQL/ABSQL/tests/files/includes.md' -%}
+    {%- include env_var("GITHUB_WORKSPACE") ~ '/tests/files/includes.md' -%}
 {% else %}
     {%- include '/ABSQL/tests/files/includes.md' -%}
 {% endif %}
